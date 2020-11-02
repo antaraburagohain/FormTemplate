c059721889ab475cf11d938e234d820e291072a3
Antara Buragohain <65369540+antaraburagohain@users.noreply.github.com>
2020-11-02 14:06:59 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from "./material/material.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeService } from './shared/employee.service';
+
+import {MatListModule} from '@angular/material/list';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
